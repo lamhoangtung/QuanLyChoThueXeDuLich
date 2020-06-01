82a860e7284fc9a66c33bea6f0065a0c11ba29c1
lamhoangtung <lamhoangtung.vz@gmail.com>
2020-06-01 11:06:44 +0700

    Try catch with minor bug fixed

--- src/main/java/com/mycompany/DAL/DALChiTietDonHang.java
+++ src/main/java/com/mycompany/DAL/DALChiTietDonHang.java
@@ -6,9 +6,12 @@
 package com.mycompany.DAL;
 import com.mycompany.DTO.DonHang;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,41 +20,51 @@ import java.util.ArrayList;
 public class DALChiTietDonHang {
     
     public List<DonHang> layChiTietDonHangTheoMa(int MaDonQ){
-        List<DonHang> arr = new ArrayList<DonHang>();
-        Connector.openConnection();
-        String query = String.format("SELECT * FROM don_hang WHERE MaDon=%d", MaDonQ);
-        Statement stmt = Connector.conn.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-        while (rs.next()){
-            int MaDon = rs.getInt("MaDon");
-            int MaKH = rs.getInt("MaKH");
-            String BienSo = rs.getString("BienSo");
-            String DiemDi = rs.getString("DiemDi");
-            String DiemDen = rs.getString("DiemDen");
-            String NgayDi = rs.getString("NgayDi");
-            String NgayVe = rs.getString("NgayVe");
-            long Gia = rs.getLong("Gia");
-            int TrangThai = rs.getInt("TrangThai");
-            DonHang temp = new DonHang(MaDon, MaKH, BienSo, DiemDi, DiemDen, NgayDi, NgayVe, Gia, TrangThai);
-            arr.add(temp);
+        try {
+            List<DonHang> arr = new ArrayList<DonHang>();
+            String query = String.format("SELECT * FROM don_hang WHERE MaDon=%d", MaDonQ);
+            Connector.openConnection();
+            Statement stmt = Connector.conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()){
+                int MaDon = rs.getInt("MaDon");
+                int MaKH = rs.getInt("MaKH");
+                String BienSo = rs.getString("BienSo");
+                String DiemDi = rs.getString("DiemDi");
+                String DiemDen = rs.getString("DiemDen");
+                String NgayDi = rs.getString("NgayDi");
+                String NgayVe = rs.getString("NgayVe");
+                long Gia = rs.getLong("Gia");
+                int TrangThai = rs.getInt("TrangThai");
+                DonHang temp = new DonHang(MaDon, MaKH, BienSo, DiemDi, DiemDen, NgayDi, NgayVe, Gia, TrangThai);
+                arr.add(temp);
+            }
+            Connector.closeConnection();
+            return arr;
+        } catch (SQLException ex) {
+            Logger.getLogger(DALChiTietDonHang.class.getName()).log(Level.SEVERE, null, ex);
         }
-        Connector.closeConnection();
-        return arr;
+        return null;
     }
     
     public void themChiTietDonHang(DonHang donHang){
-        String query = String.format("INSERT INTO don_hang VALUES(%d, %d, %s, %s, %s, %s, %s, %d, %d)",
-                donHang.getMaDon(),
-                donHang.getMaKH(),
-                donHang.getBienSo(),
-                donHang.getDiemDi(),
-                donHang.getDiemDen(),
-                donHang.getNgayDi(),
-                donHang.getNgayVe(),
-                donHang.getGia(),
-                donHang.getTrangThai());
-        Statement stmt = Connector.conn.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-        Connector.closeConnection();
+        try {
+            String query = String.format("INSERT INTO don_hang VALUES(%d, %d, %s, %s, %s, %s, %s, %d, %d)",
+                    donHang.getMaDon(),
+                    donHang.getMaKH(),
+                    donHang.getBienSo(),
+                    donHang.getDiemDi(),
+                    donHang.getDiemDen(),
+                    donHang.getNgayDi(),
+                    donHang.getNgayVe(),
+                    donHang.getGia(),
+                    donHang.getTrangThai());
+            Connector.openConnection();
+            Statement stmt = Connector.conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            Connector.closeConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(DALChiTietDonHang.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
