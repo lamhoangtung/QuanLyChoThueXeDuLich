8fbb8a400822fcc8ef95895c4987e6ba7296f0cf
lamhoangtung <lamhoangtung.vz@gmail.com>
2020-06-01 10:08:13 +0700

    Init database connector of DAL (Following MVC partten)

---
+++ src/main/java/com/mycompany/DAL/Connector.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.DAL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author linus
+ */
+public class Connector {
+    public static Connection conn = null;
+    private static String user = "root";
+    private static String password = "123456";
+
+
+    public static void openConnection() {
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Connector.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ql_thuexe_minhlong", Connector.user, Connector.password);
+            System.out.println("Ket noi CSDL thanh cong");
+            
+        }catch(Exception ex){
+            System.out.println(ex.toString());
+        }
+    }
+    
+    public static void closeConnection(){
+        try{
+            Connector.conn.close();
+            System.out.println("Dong ket noi CSDL thanh cong");
+            
+        }catch(Exception ex){
+            System.out.println(ex.toString());
+        }
+    }
+    
+    
+}
